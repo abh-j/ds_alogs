97d4df607b47b48f08a7f1d0883e8fce7a537aa1
Abhishek J <64753798+abh-j@users.noreply.github.com>
2022-01-19 14:10:40 +0530

    stack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('=======')
+// Stack
+// Queues
+// Linked List
+// Set
+// Hash table
+// Tree
+// Graph
+
+
+import Stack from './Stack'
+const stack = new Stack();
+
+stack.push(1)
+stack.push(2)
+stack.push(7)
+stack.push(5)
+console.log(stack);
+
